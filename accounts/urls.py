6979503131dd9fb2921f630590a286a8cb356daa
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2020-07-22 14:46:46 +0430

    Fix signout error

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'core'
 urlpatterns = [
     path('', views.sign_in, name='sign_in'),
     path('sign_out/', views.sign_out, name='sign_out'),
     path('register/', views.register, name='register'),
-
-
 ]
