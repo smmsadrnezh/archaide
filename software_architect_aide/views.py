28033615f5af4cefb406a0892635236fd876161f
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2020-07-22 19:39:55 +0430

    Write modal for delete button

--- software_architect_aide/views.py
+++ software_architect_aide/views.py
@@ -15,12 +15,6 @@ def dashboard(request):
 
 
 @login_required(login_url='/')
-def instantiate(request):
-    context = {'': '', }
-    return render(request, 'dashboard_instantiate.html', context)
-
-
-@login_required(login_url='/')
 def tradeoff(request):
     context = {'': '', }
     return render(request, 'dashboard_tradeoff.html', context)
@@ -43,6 +37,9 @@ def architecture_create(request):
         name = request.POST.get('name', '')
         Architecture.objects.create(name=name, owner=request.user)
         return render(request, 'dashboard_home.html', {'success': 'True'})
+    else:
+        context = {'': '', }
+        return render(request, 'dashboard_architecture_create.html', context)
 
 
 @login_required(login_url='/')
