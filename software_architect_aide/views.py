94fd7d13ce0a6663772affbb6d548a11148467ad
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2020-07-23 09:50:58 +0430

    Revert "Implement visualize feature"
    This reverts commit fe6defa1

--- software_architect_aide/views.py
+++ software_architect_aide/views.py
@@ -1,11 +1,8 @@
-import os
-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rdflib import Graph
 
 from software_architect_aide.models import Architecture
-from software_architect_aide.settings import MEDIA_ROOT
 
 
 @login_required(login_url='/')
@@ -38,17 +35,13 @@ def architecture_create(request):
         if request.FILES.get('ontology'):
             architecture.owl_file = request.FILES.get('ontology')
             architecture.save()
-            image_path = os.path.join(MEDIA_ROOT, 'visual', architecture.owl_file.name.split('/')[-1] + '.png')
-            rdf_path = architecture.owl_file.path
-            rdf_graph = Graph().parse(rdf_path)
-            architecture.axiom_count = len(rdf_graph)  # visualize(rdf_path, image_path)
+            architecture.axiom_count = len(Graph().parse(architecture.owl_file.path))
         else:
             pass
         architecture.save()
-        context = {'success': True}
-        return render(request, 'dashboard_architecture_create.html', context)
+        return redirect('dashboard')
     else:
-        context = {'': ''}
+        context = {'': '', }
         return render(request, 'dashboard_architecture_create.html', context)
 
 
