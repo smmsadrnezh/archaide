1ed4f307fd08e4a7243cb183d46b2661c90e6501
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2020-07-23 10:53:53 +0430

    Revert "Implement visualize feature"
    This reverts commit 8e11732b

---
+++ software_architect_aide/common.py
@@ -0,0 +1,21 @@
+import networkx as nx
+from matplotlib.pyplot import plot as plt
+from rdflib import Graph
+from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
+
+
+def visualize(rdf_path, image_path):
+    rdf_graph = Graph().parse(rdf_path)
+    G = rdflib_to_networkx_multidigraph(rdf_graph)
+
+    # Plot Networkx instance of RDF Graph
+    pos = nx.spring_layout(G, scale=2)
+    edge_labels = nx.get_edge_attributes(G, 'r')
+    nx.draw_networkx_edge_labels(G, pos, labels=edge_labels)
+    nx.draw(G, with_labels=True)
+    plt.savefig(image_path)
+
+
+def axiom_count(rdf_path):
+    rdf_graph = Graph().parse(rdf_path)
+    return len(rdf_graph)
