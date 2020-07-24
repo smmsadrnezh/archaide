dc2d4a96d2b9e58004674bab6ed4cb76421bea59
milad <miladsj1994@gmail.com>
2020-07-24 10:53:37 +0430

    move captcha to register form

--- accounts/views.py
+++ accounts/views.py
@@ -10,31 +10,18 @@ from django.shortcuts import render
 def sign_in(request):
     context = {}
     if request.method == "POST":
-        ''' Begin reCAPTCHA validation '''
-        recaptcha_response = request.POST.get('g-recaptcha-response')
-        data = {
-            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-            'response': recaptcha_response
-        }
-        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-        result = r.json()
-        ''' End reCAPTCHA validation '''
-        if result['success']:
-            context['true_captcha'] = True
-            try:
-                user = authenticate(username=User.objects.get(email=request.POST.get('email', '')).username,
-                                    password=request.POST.get('password', ''))
-            except ObjectDoesNotExist:
-                context['success'] = False
-                return render(request, 'sign_in.html', context)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-            else:
-                context['success'] = False
-                return render(request, 'sign_in.html', context)
+        try:
+            user = authenticate(username=User.objects.get(email=request.POST.get('email', '')).username,
+                                password=request.POST.get('password', ''))
+        except ObjectDoesNotExist:
+            context['success'] = False
+            return render(request, 'sign_in.html', context)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
         else:
-            context['true_captcha'] = False
+            context['success'] = False
+            return render(request, 'sign_in.html', context)
     else:
         if request.user.is_authenticated:
             return redirect('dashboard')
@@ -49,18 +36,41 @@ def sign_out(request):
 
 
 def register(request):
+    context = {}
     if request.method == 'POST':
         password = request.POST['password']
         password2 = request.POST['password2']
         email = request.POST['email']
 
-        if password2 != password:
-            return render(request, 'register.html', {'not_match_password': True})
+        ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+        data = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+        result = r.json()
+        ''' End reCAPTCHA validation '''
 
-        if User.objects.filter(email=email).exists():
-            return render(request, 'register.html', {'duplicate_user': True})
+        if result['success']:
+            context['true_captcha'] = True
 
-        User.objects.create_user(username=email, email=email, password=password)
-        return render(request, 'sign_in.html', {'success': True, 'register_success': True})
+            if password2 != password:
+                context['not_match_password'] = True
+                return render(request, 'register.html', context)
+
+            if User.objects.filter(email=email).exists():
+                context['duplicate_user'] = True
+                return render(request, 'register.html', context)
+
+            context['success'] = True
+            context['register_success'] = True
+            User.objects.create_user(username=email, email=email, password=password)
+            return render(request, 'sign_in.html', context)
+        else:
+            context['true_captcha'] = False
+            return render(request, 'register.html', context)
     else:
-        return render(request, 'register.html', {'success': True})
+        context['true_captcha'] = True
+        context['success'] = True
+        return render(request, 'register.html', context)
