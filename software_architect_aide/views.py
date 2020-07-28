f8fad5b51fd60467a944b462d339765e1df4dfcf
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2020-07-28 23:06:51 +0430

    Implement reference architecture creation

--- software_architect_aide/views.py
+++ software_architect_aide/views.py
@@ -136,10 +136,10 @@ def create_reference(request):
             print(query_part1)
             query_result = query_reference(query_part1)
             quality_tactics = pars_query_two_label(query_result)
-            result = dict()
+            quality_tactics_dict = {quality_tactic[0]: [] for quality_tactic in quality_tactics}
             for quality_tactic in quality_tactics:
-                result[quality_tactic[0]].append(quality_tactic[1])
-            context = {'current_step': current_step + 1, 'quality_tactics': quality_tactics}
+                quality_tactics_dict[quality_tactic[0]].append(quality_tactic[1])
+            context = {'current_step': current_step + 1, 'quality_tactics_dict': quality_tactics_dict}
 
         elif current_step == 2:
             #         # quality_attributes = request.POST.get('quality_attributes')
