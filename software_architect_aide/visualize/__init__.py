1cd923edb1239b837bd0514d428ba1a3f43bb30c
milad <miladsj1994@gmail.com>
2020-07-29 15:19:15 +0430

    remove labels from visualize

--- software_architect_aide/visualize/__init__.py
+++ software_architect_aide/visualize/__init__.py
@@ -1,11 +1,14 @@
 import argparse
-
+import json
+from software_architect_aide.local_settings import BASE_DIR
 from software_architect_aide.visualize.ontology_viz import OntologyGraph
 from software_architect_aide.visualize.utils import Config
 
+CONFIG_PATH = BASE_DIR + '/software_architect_aide/visualize/config.json'
+
 
 def main(rdf_path, dot_path):
-    args = argparse.Namespace(config='config.json', files=[rdf_path], format='xml', out=dot_path, ontology=None)
+    args = argparse.Namespace(config=CONFIG_PATH, files=[rdf_path], format='xml', out=dot_path, ontology=None)
     config = Config(args.config)
     og = OntologyGraph(args.files, config, args.format, ontology=args.ontology)
     og.write_file(args.out)
